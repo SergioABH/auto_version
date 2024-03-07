863e1dfd7a469ff0de46990b3a7955318c4717e4
SergioABH <bedoyasergio3@gmail.com>
2024-03-07 12:06:59 -0500

    Test new pipeline - feat/example

--- .github/scripts/reintegrate_changes.sh
+++ .github/scripts/reintegrate_changes.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -e
 
-echo "Reintegrating Changes"
-if [[ $GITHUB_EVENT_NAME == 'pull_request' && $GITHUB_EVENT_ACTION == 'closed' && $GITHUB_EVENT_PULL_REQUEST_MERGED == 'true' && $GITHUB_EVENT_PULL_REQUEST_BASE_REF == 'master' ]]; then
+echo "Reintegrate Changes"
+base_branch=$Determine_Version_BASE_BRANCH
+branch_name=$Determine_Version_BRANCH_NAME
+github_event_action=$github_event_action
+github_event_pull_request_merged=$github_event_pull_request_merged
+github_event_name=$github_event_name
+github_event_pull_request_base_ref=$github_event_pull_request_base_ref
+
+if [[ $github_event_name == 'pull_request' && $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' && $github_event_pull_request_base_ref == 'master' ]]; then
 
     version=$(git describe --tags --abbrev=0 $(git rev-list --tags --max-count=1 master))
     reintegrate_branch="reintegrate/$version"
-
-    git config --global user.email "actions@github.com"
-    git config --global user.name "GitHub Actions"
+    
     git fetch origin master
     git checkout -b $reintegrate_branch master
     git push origin $reintegrate_branch
 
     PR_TITLE="Reintegrate $version to dev"
 
-    # Revisa el estado de las ramas para depuración
-    git branch -a
-
-    # Crea la solicitud de extracción
     curl -X POST \
         -H "Authorization: Bearer $GH_TOKEN" \
         -d "{\"title\":\"$PR_TITLE\",\"head\":\"$reintegrate_branch\",\"base\":\"dev\"}" \
         "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls"
 
-    # Revisa si la solicitud de extracción se creó correctamente
     echo "Pull request created successfully"
 fi
