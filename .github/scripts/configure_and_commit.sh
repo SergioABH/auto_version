94b03cb8c687715d010f3d782deeefbafbab9072
SergioABH <bedoyasergio3@gmail.com>
2024-03-07 13:56:14 -0500

    Test new pipeline - feat/example

--- .github/scripts/configure_and_commit.sh
+++ .github/scripts/configure_and_commit.sh
@@ -5,18 +5,35 @@ echo "Configuring Git"
 git config --global user.email "actions@github.com"
 git config --global user.name "GitHub Actions"
 
+# Función para obtener la versión de package.json de una rama
+get_version_from_branch() {
+    local branch_name=$1
+    local package_version
+
+    # Obtener la versión de package.json de la rama correspondiente
+    if [[ $branch_name == 'dev' || $branch_name == 'qa' ]]; then
+        package_version=$(git show $branch_name:package.json | jq -r .version)
+    else
+        package_version=$(node -pe "require('./package.json').version")
+    fi
+
+    echo $package_version
+}
+
 echo "Determining Version"
 base_branch=$Determine_Version_BASE_BRANCH
 branch_name=$Determine_Version_BRANCH_NAME
 github_event_action=$github_event_action
 github_event_pull_request_merged=$github_event_pull_request_merged
 
+# Obtener las versiones de QA y DEV
+qa_version=$(get_version_from_branch 'qa')
+dev_version=$(get_version_from_branch 'dev')
+
 if [[ $base_branch == 'qa' ]]; then
     if [[ $branch_name == 'dev' ]]; then
         if [[ $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' ]]; then
-            dev_version=$(git show $base_branch:package.json | jq -r .version)
-            qa_version=$(git show $base_branch:package.json | jq -r .version)
-
+            # Check if the minor version is equal to the QA minor version
             minor_version=$(echo $dev_version | cut -d. -f2)
             qa_minor_version=$(echo $qa_version | cut -d. -f2)
             
