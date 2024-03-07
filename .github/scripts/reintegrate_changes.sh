48bb574c2446f94483400de7d9a718410244ff61
SergioABH <bedoyasergio3@gmail.com>
2024-03-07 10:42:31 -0500

    Test new pipeline - feat/example

--- .github/scripts/reintegrate_changes.sh
+++ .github/scripts/reintegrate_changes.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-#!/bin/bash
-set -e
-
 echo "Reintegrating Changes"
 if [[ $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' && $github_event_pull_request_base_ref == 'master' ]]; then
 
     version=$(git describe --tags --abbrev=0 $(git rev-list --tags --max-count=1 master))
     reintegrate_branch="reintegrate/$version"
-
+    
+    git config --global user.email "actions@github.com"
+    git config --global user.name "GitHub Actions"
     git fetch origin master
     git checkout -b $reintegrate_branch master
     git push origin $reintegrate_branch
