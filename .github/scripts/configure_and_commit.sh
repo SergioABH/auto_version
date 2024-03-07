8a892ed01dde0248457cd00ce7f5a1d0a5c7ee58
SergioABH <bedoyasergio3@gmail.com>
2024-03-07 09:11:18 -0500

    Test new pipeline - feat/example

--- .github/scripts/configure_and_commit.sh
+++ .github/scripts/configure_and_commit.sh
@@ -6,30 +6,25 @@ git config --global user.email "actions@github.com"
 git config --global user.name "GitHub Actions"
 
 echo "Determining Version"
-Determine_Version_BASE_BRANCH=$(jq -r .pull_request.base.ref $GITHUB_EVENT_PATH)
-Determine_Version_BRANCH_NAME=$(jq -r .pull_request.head.ref $GITHUB_EVENT_PATH)
+base_branch=$Determine_Version_BASE_BRANCH
+branch_name=$Determine_Version_BRANCH_NAME
 
-echo "Determine_Version_BASE_BRANCH=$Determine_Version_BASE_BRANCH" >> $GITHUB_ENV
-echo "Determine_Version_BRANCH_NAME=$Determine_Version_BRANCH_NAME" >> $GITHUB_ENV
-echo "github_event_action=${{ github.event.action }}" >> $GITHUB_ENV
-echo "github_event_pull_request_merged=${{ github.event.pull_request.merged }}" >> $GITHUB_ENV
-
-if [[ $Determine_Version_BASE_BRANCH == 'qa' ]]; then
-    if [[ $Determine_Version_BRANCH_NAME == 'dev' ]]; then
+if [[ $base_branch == 'qa' ]]; then
+    if [[ $branch_name == 'dev' ]]; then
         if [[ $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' ]]; then
             npm --no-git-tag-version version prerelease --preid=beta
         fi
-    elif [[ $Determine_Version_BRANCH_NAME == *fix/* ]]; then
+    elif [[ $branch_name == *fix/* ]]; then
         if [[ $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' ]]; then
             npm version prepatch --preid=beta
         fi
     fi
-elif [[ $Determine_Version_BASE_BRANCH == 'master' ]]; then
-    if [[ $Determine_Version_BRANCH_NAME == 'qa' ]]; then
+elif [[ $base_branch == 'master' ]]; then
+    if [[ $branch_name == 'qa' ]]; then
         if [[ $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' ]]; then
             npm version minor
         fi
-    elif [[ $Determine_Version_BRANCH_NAME == *fix/* ]]; then
+    elif [[ $branch_name == *fix/* ]]; then
         if [[ $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' ]]; then
             npm version patch
         fi
@@ -39,17 +34,17 @@ fi
 echo "Getting New Version"
 version=$(npm version)
 
-echo "Base branch: $Determine_Version_BASE_BRANCH"
-echo "Branch name: $Determine_Version_BRANCH_NAME"
+echo "Base branch: $base_branch"
+echo "Branch name: $branch_name"
 echo "Version: $version"
 
-git fetch origin $Determine_Version_BASE_BRANCH:$Determine_Version_BASE_BRANCH || true
-git checkout $Determine_Version_BASE_BRANCH || true
+git fetch origin $base_branch:$base_branch || true
+git checkout $base_branch || true
 
 git add .
 git commit -am "Update version" || true
-git checkout $Determine_Version_BASE_BRANCH
-git push origin $Determine_Version_BASE_BRANCH --follow-tags || true
+git checkout $base_branch
+git push origin $base_branch --follow-tags || true
 
 echo "Reintegrating Changes"
 if [[ $github_event_action == 'closed' && $github_event_pull_request_merged == 'true' && $github_event_pull_request_base_ref == 'master' ]]; then
